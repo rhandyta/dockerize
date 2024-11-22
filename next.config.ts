c7e20789fa352ae7697174b80ba3bc7819c1e674
axongcong <axongcong@gmail.com>
2024-11-22 16:23:18 +0700

    docker

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone'
+  output: 'standalone',
+  serverRuntimeConfig: {
+    host: '0.0.0.0',
+  },
+
 };
 
 export default nextConfig;
